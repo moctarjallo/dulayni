598b215aa86c518c380044d94b0a910feffc5c19
Moctar <moctarjallo@gmail.com>
2025-07-27 03:57:56 +0000

    Introduce cli argument --query

--- src/dulayni/cli.py
+++ src/dulayni/cli.py
@@ -8,6 +8,7 @@ from dulayni.mcp.client import run_agent
 @click.option("--model", "-m", default="gpt-4.1-mini",
               type=click.Choice(["gpt-4.1", "gpt-4.1-mini"]))
 @click.option("--openai_api_key", "-k", required=True, envvar="OPENAI_API_KEY")
+@click.option("--query", "-q", type=str, required=False)
 @click.option("--path2mcp_servers_file", "-mcp", default="config/mcp_servers.json",
               type=click.Path(exists=True))
 @click.option("--startup_timeout", "-t", default=10.0, type=float)
@@ -16,17 +17,19 @@ from dulayni.mcp.client import run_agent
 @click.option("--print_mode", default="rich",
               type=click.Choice(["json", "rich"]))
 def main(model: str, openai_api_key: str,
+         query: str,
          path2mcp_servers_file: str,
          startup_timeout: float,
          parallel_tool_calls: bool,
          agent_type: str,
          print_mode: str):
     # You can load the API key in env or pass it into run_agent if needed
+    query = query or "what's (3 + 5) x 12?"
     result = asyncio.run(run_agent(
         agent_type=agent_type,
         role="user",
         model=model,
-        content="what's (3 + 5) x 12?",
+        content=query,
         system_prompt="You are a helpful agent",
         thread_id="123",
         memory_db="dulayni_memory.sqlite",

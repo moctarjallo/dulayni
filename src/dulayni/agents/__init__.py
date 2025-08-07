18cb34faddd6224f1be027ad4f7b557f431c8f61
Moctar <moctarjallo@gmail.com>
2025-08-07 01:35:08 +0000

    Introduce deepagent

--- src/dulayni/agents/__init__.py
+++ src/dulayni/agents/__init__.py
@@ -1,7 +1,8 @@
-from .react import ReactAgent
+from .react import ReactAgent, DeepAgent
 
 async def create_agent(agent_type, role, model, system_prompt, mcp_servers_file, checkpointer, parallel_tool_calls):
-    if agent_type=="react": # change this to new added type of agent
-        return await ReactAgent.create(role, model, system_prompt, mcp_servers_file, checkpointer, parallel_tool_calls)
+    if agent_type=="deep_agent": # change this to new added type of agent
+        agent = await DeepAgent.create(role, model, system_prompt, mcp_servers_file, checkpointer, parallel_tool_calls)
     else: # by default
-        return await ReactAgent.create(role, model, system_prompt, mcp_servers_file, checkpointer, parallel_tool_calls)
+        agent = await ReactAgent.create(role, model, system_prompt, mcp_servers_file, checkpointer, parallel_tool_calls)
+    return agent
